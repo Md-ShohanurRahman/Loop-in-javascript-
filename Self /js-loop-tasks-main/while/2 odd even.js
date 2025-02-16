8cece72f288a43ee490825b1a950d0e54136f06c
Md-ShohanurRahman <shohanurrahmanibnaymizan@gmail.com>
2025-02-16 09:41:42 +0600

    add some for loop related code

--- Self /js-loop-tasks-main/while/2 odd even.js
+++ Self /js-loop-tasks-main/while/2 odd even.js
@@ -16,3 +16,24 @@ Find all the even numbers from 78 to 98.
  */
 
 /*programming hero*/
+console.log("odd numbers from 61 to 100");
+
+let i=61;
+while(i<=100){
+   
+    if(i%2===1){
+        console.log(i);
+    }
+    i++; 
+}
+
+console.log("even numbers from 78 to 98");
+let n=78;
+while(n<=98){
+   
+    if(n%2===0){
+        console.log(n);
+    }
+    n++;
+ 
+}

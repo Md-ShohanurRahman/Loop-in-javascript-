c87d04da1463d537a5b1d54889f81b90b87646a5
Md-ShohanurRahman <shohanurrahmanibnaymizan@gmail.com>
2025-02-16 10:00:44 +0600

    add the code of finding sumof even and odd numbers using while loop

--- Self /js-loop-tasks-main/while/3 sum of odd numbers.js
+++ Self /js-loop-tasks-main/while/3 sum of odd numbers.js
@@ -11,8 +11,28 @@ Display sum of all the odd numbers from 81 to 131.
 
 Subtask-2:
 
-Display sum of all the even numbers from 206 to 311.
+Display .
 
  */
 
 /*programming hero*/
+
+let sum=0;
+let i=81;
+while(i<=131){
+    if (i%2===1) {
+        sum=sum+i;
+    }
+    i++;
+}
+console.log("sum of all the odd numbers from 81 to 131",sum);
+
+let total=0;
+let n=206;
+while(n<=311){
+    if (n%2===0) {
+        total=total+n;
+    }
+    n++;
+}
+console.log("sum of all even numbers from 206 to 311",total)

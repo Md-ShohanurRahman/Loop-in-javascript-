4cabe0209596d706be595e3d80f0fafd7bc41cb4
Md-ShohanurRahman <shohanurrahmanibnaymizan@gmail.com>
2025-02-16 10:41:32 +0600

    add some code where break key word is used to solve the problem

--- Self /js-loop-tasks-main/break/3 square break.js
+++ Self /js-loop-tasks-main/break/3 square break.js
@@ -1,3 +1,15 @@
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+
+
+
+for(let i =1;i<=100;i++){
+    let squareNumber=i*i;
+  
+    if(squareNumber<=100){
+        console.log(squareNumber);
+        break
+    }
+ 
+}
